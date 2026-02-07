b6d4a6a3e0e65cf9e51d0cefc76596c40f9a03a6
Hinduja30 <hinduja5660@gmail.com>
2026-02-07 19:11:42 +0530

    firstcommit

---
+++ UNIT1/Singly_LinkedList/Insert_AtPosition.c
@@ -0,0 +1,150 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *link;
+}*NODE;
+
+NODE create_node(int data)
+{
+   NODE newNode = malloc(sizeof(struct Node));
+   if(newNode == NULL)
+   {
+    printf("Memory not allocated successfully\n");
+    exit(1);
+   } 
+   newNode->data = data;
+   newNode->link = NULL;
+
+   return newNode;
+}
+
+NODE Insert_Front(NODE Head , int data)
+{
+    NODE newNode = create_node(data);
+    if(newNode != NULL)
+    {
+        newNode->link = Head;
+    }
+    return newNode;
+}
+
+NODE Insert_rear(NODE Head ,int data)
+{
+   NODE newNode = create_node(data);
+
+    if(Head == NULL)
+    {
+        return newNode;
+    }
+   NODE temp = Head;
+   while(temp->link != NULL)
+   {
+    temp=temp->link;
+   }
+   temp->link = newNode; 
+   return Head;
+}
+
+NODE Insert_atPos(NODE Head , int pos , int data)
+{
+    NODE newNode = create_node(data);
+    if(Head == NULL)
+    {
+        return newNode;
+    }
+    if(pos==0)
+    {
+        return(Insert_Front(Head,data));
+    }
+    
+    NODE temp = Head;
+    int position = 1;
+    while(temp != NULL && position < (pos-1))
+    {
+        temp = temp->link;
+        position++;
+    }
+     if(temp == NULL)
+    {
+        printf("Out of bound and will be inserted at end\n");
+        return(Insert_rear(Head,data));
+    }
+    newNode->link = temp->link;
+    temp->link = newNode;
+
+    return Head;
+}
+
+void Display(NODE Head)
+{
+    if(Head == NULL)
+    {
+        printf("List is Empty\n");
+    }
+    NODE temp = Head;
+    while(temp != NULL)
+    {
+        if(temp->link == NULL)
+        {
+            printf("%d\n",temp->data);
+        }
+        else{
+        printf("%d -> ",temp->data);
+        }
+        temp = temp->link;
+    }
+}
+
+NODE delete_Rear(NODE Head)
+{
+    if(Head == NULL)
+    {
+        printf("The list is Empty\n");
+    }
+    if(Head->link == NULL)
+    {
+        printf("deleted %d\n",Head->data);
+    }
+    NODE temp = Head;
+    NODE prev = NULL;
+    while(temp -> link != NULL)
+    {   
+        prev = temp;
+        temp = temp-> link;
+    }
+    printf("deleted Node is %d\n",temp->data);
+    prev->link = NULL;
+    free(temp);
+
+    return Head;
+}
+
+int main()
+{
+    NODE node = NULL;
+    node = Insert_Front(node,1);
+    node = Insert_Front(node,2);
+    node = Insert_Front(node,3);
+    node = Insert_Front(node,4);
+    node = Insert_Front(node,5);
+
+    Display(node);
+
+    node = delete_Rear(node);
+
+    Display(node);
+
+    node = Insert_atPos(node , 3 ,10);
+    Display(node);
+
+    node = Insert_atPos(node , 0 ,20);
+    Display(node);
+
+    node = Insert_atPos(node , 10 ,80);
+    Display(node);
+
+    return 0;
+}
