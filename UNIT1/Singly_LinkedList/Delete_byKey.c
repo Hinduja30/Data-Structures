3e2aae7b0f1a2b87301c74d091e822f54d8e7d0f
Hinduja30 <hinduja5660@gmail.com>
2026-02-08 13:09:45 +0530

    firstcommit

---
+++ UNIT1/Singly_LinkedList/Delete_byKey.c
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node
+{
+    int data;
+    struct Node *link;
+}*NODE;
+
+NODE create_Node(int data)
+{
+    NODE newNode = malloc(sizeof(struct Node));
+
+    if(newNode == NULL)
+    {
+        printf("Memory not allocated successfully");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->link = NULL;
+
+    return newNode;
+}
+
+NODE insert_Front(NODE Head , int data)
+{
+    NODE newNode = create_Node(data);
+    if(newNode != NULL)
+    {
+        newNode->link = Head;
+    }
+    return newNode;
+}
+
+NODE Delete_ByKey(NODE Head , int key)
+{
+    if(Head == NULL)
+    {
+        printf("Cant be deleted\n");
+        return NULL;
+    }
+    if(Head->data ==  key)
+    {
+        NODE temp =Head;
+        Head = Head->link;
+        printf("\nDeleted Node %d",temp->data);
+        free(temp);
+        return Head;
+    }
+     
+    NODE curr = Head;
+    while(curr->link != NULL && curr->link->data != key )
+    {
+        curr = curr-> link;
+    }
+    NODE temp = curr->link;
+    curr->link = temp->link;
+    printf("Deleted Node is %d\n",temp->data);
+    free(temp);
+
+    return Head;
+}
+
+
+
+void display(NODE Head)
+{
+    if(Head == NULL)
+    {
+        printf("Empty list\n");
+        return;
+    }
+    else
+    {
+        printf("HEAD -> ");
+        NODE temp = Head;
+        while(temp != NULL)
+        {
+            if(temp->link == NULL){
+                printf("%d",temp->data);
+            }
+            else
+            {
+                printf("%d -> ",temp->data);
+            }
+            temp = temp -> link;
+        }
+        printf("-> NULL\n");
+    }
+
+}
+
+int main()
+{
+    NODE node = NULL;
+    node = insert_Front(node,1);
+    node = insert_Front(node,2);
+    node = insert_Front(node,3);
+    node = insert_Front(node,4);
+    node = insert_Front(node,5);
+
+    display(node);
+
+    node = Delete_ByKey(node , 4);
+    display(node);
+
+    node = Delete_ByKey(node , 1);
+    display(node);
+    return 0;
+}
