1262322a0172d93dadb3851b9335c41d5aedff96
Hinduja30 <hinduja5660@gmail.com>
2026-02-08 13:23:45 +0530

    first Commit

---
+++ UNIT1/Doubly_LinkedList/Insert_Front.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct DNode
+{
+    int data;
+    struct DNode *left , *right;
+}*DNODE;
+
+DNODE Create_DNode(int data)
+{
+    DNODE newNode = malloc(sizeof(struct DNode));
+    if(newNode == NULL)
+    {
+        printf("Memory not allocated successfully\n");
+        exit(1);
+    }
+        newNode->data = data;
+        newNode->left = newNode->right = NULL;
+
+        return newNode;
+    
+}
+
+DNODE Insert_Front(DNODE Head , int data)
+{
+    DNODE newNode = Create_DNode(data);
+    newNode->right = Head;
+    if(Head != NULL)
+    {
+    Head->left = newNode;
+    }
+    return newNode;
+}
+
+void Display(DNODE Head)
+{
+    if(Head == NULL)
+    {
+        printf("The list is empty\n");
+        return;
+    }
+    DNODE curr = Head;
+    printf("HEAD -> ");
+    while(curr != NULL)
+    {
+       printf("%d -> ",curr->data);
+       curr = curr->right;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    DNODE node = NULL;
+    node = Insert_Front(node , 1);
+    node = Insert_Front(node , 2);
+    node = Insert_Front(node , 3);
+    node = Insert_Front(node , 4);
+    node = Insert_Front(node , 5);
+    Display(node);
+}
